fe7df899ac89be9ca9f9166374c5d001b459f45c
hillmydyan <hillmy03@gmail.com>
2025-12-13 20:22:22 +0700

    chore: add .gitignore and untrack venv/__pycache__ files; update lab_controller and ddos template

--- controllers/lab_controller.py
+++ controllers/lab_controller.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, redirect
-from services.guacamole_service import get_guacamole_token
+from flask import Blueprint, render_template
 from datetime import datetime
 import os
 
 lab = Blueprint("lab", __name__)
 
-# Pastikan folder logs ada
 if not os.path.exists("logs"):
     os.makedirs("logs")
 
@@ -14,10 +12,18 @@ def simpan_log(aksi):
         waktu = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"{waktu} - {aksi}\n")
 
-from flask import Blueprint, render_template
-
-lab = Blueprint("lab", __name__)
-
+# =========================
+# LAB DDOS
+# =========================
 @lab.route("/lab/ddos")
 def ddos_design():
+    simpan_log("Akses LAB DDOS")
     return render_template("lab/ddos.html")
+
+# =========================
+# LAB SNIFFING
+# =========================
+@lab.route("/lab/sniffing")
+def sniffing_design():
+    simpan_log("Akses LAB Sniffing")
+    return render_template("lab/sniffing.html")
